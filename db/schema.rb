6f341883a5855806ddfa012f8627745f5f7f0fe6
John Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2024-11-04 23:15:10 -0800

    Migrate Expense database

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_05_064627) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_05_071419) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "expenses", force: :cascade do |t|
+    t.string "name"
+    t.decimal "amount"
+    t.text "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_expenses_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,4 +35,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_05_064627) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "expenses", "users"
 end
